295dedba021e8809031f20e961b093c7b8b12693
Steve Loveless <steve.loveless@gmail.com>
2013-03-18 21:11:37 -0700

    Added epel part

---
+++ lib/drama/parts/epel.rb
@@ -0,0 +1,15 @@
+class Epel < Drama::Part
+  def act(os_version: 5, arch: 'x86_64')
+    rpm_version = os_version == 5 ? '5-4' : '6-8'
+    rpm_destination = "/tmp/epel-release-#{rpm_version}.noarch.rpm"
+    rpm_source = "http://dl.fedoraproject.org/pub/epel/#{os_version}/#{arch}/epel-release-#{rpm_version}.noarch.rpm"
+
+    host.rpm package: 'epel', on_fail: -> {
+      host.file path: rpm_destination, source: rpm_source
+      host.rpm package: rpm_destination, sudo: true
+    }
+
+    host.yum update_cache: true, sudo: true
+    host.yum upgrade: true, sudo: true
+  end
+end
